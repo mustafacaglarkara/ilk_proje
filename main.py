2ab92a07b4b33720f5fbd334617339cc4156278e
Mustafa Çağlar KARA <mustafacaglarkara@gmail.com>
2024-10-01 14:25:01 +0300

    Show Led Icon

---
+++ main.py
@@ -0,0 +1,4 @@
+def on_forever():
+    basic.show_icon(IconNames.HEART)
+    basic.show_icon(IconNames.ANGRY)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
-basic.forever(function () {
-	
+basic.forever(function on_forever() {
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Angry)
 })
